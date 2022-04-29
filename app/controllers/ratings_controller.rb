17e5b07bac0b385ccdd60c2bb9fe31468f708d58
AnitaPr <anitabllaca88@gmail.com>
2022-04-28 23:01:51 -0400

    adding methods in the ratings  controller

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,16 +1,34 @@
 class RatingsController < ApplicationController
 def index
+  ratings = Rating.all
+  render json: ratings.as_json
 end
 
 def create
+  rating = Rating.new(
+    user_id: params[:user_id],
+    location_id: params[:location_id],
+    rating: params[:rating]
+  )
+  rating.save
+  render json: rating.as_json
 end 
 
 def show 
+  rating = Rating.find_by(id: params[:id])
+  render json: rating.as_json
 end 
 
 def update
+ rating = Rating.find_by(id: params[:id])
+  rating.user_id = params[:user_id],
+  rating.location_id = params[:location_id],
+  rating.rating = params[:rating]
 end 
 
 def destroy
+  rating = Rating.find_by(id: params[:id])
+  rating.destroy
+  render json: {message: "Rating successfully destroyed."}
 end 
 end
