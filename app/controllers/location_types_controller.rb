b19981ef0dc2dea0ce41b7471195d45e7f7fb8c4
AnitaPr <anitabllaca88@gmail.com>
2022-04-28 21:51:23 -0400

    adding methods in the user controller

--- app/controllers/location_types_controller.rb
+++ app/controllers/location_types_controller.rb
@@ -1,2 +1,3 @@
 class LocationTypesController < ApplicationController
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+
+def index
+  users = User.all
+  render json: users.as_json
+end
+
+def create
+ user = User.new(
+  name: params[:name],
+  email: params[:email],
+  password_digest: params[:password_digest]
+ )
+ user.save
+ render json: user.as_json
+end
+
+def show 
+ user = User.find_by(id: params[:id])
+ render json: user.as_json
+end
+
+def update
+ user = User.find_by(id: params[:id])
+ user.name = params[:name] || user.name
+ user.email = params[:email] || user.email
+ user.password_digest = params[:password_digest] || user.password_digest
+end 
+
+def destroy
+  user = User.find_by(id: params[:id])
+  user.destroy
+  render json: {message: "User successfully destroyed."}
+end 
 end
